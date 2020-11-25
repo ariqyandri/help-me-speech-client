f5b47c410c22d702069dd70d2218f6c938c562b8
ariqyandri <ariq140199@gmail.com>
2020-11-25 17:21:24 +0100

    Update user store, add action and dispatches

--- src/store/user/reducer.ts
+++ src/store/user/reducer.ts
@@ -8,6 +8,14 @@ const initialState = {
 
 export default (state = initialState, action: Action) => {
   switch (action.type) {
+    case "LOGIN_SUCCESS":
+      localStorage.setItem("token", action.payload.token);
+      return { ...state, ...action.payload };
+    case "LOG_OUT":
+      localStorage.removeItem("token");
+      return { ...initialState, token: null };
+    case "TOKEN_STILL_VALID":
+      return { ...state, ...action.payload };
     default:
       return state;
   }
--- src/store/user/types.ts
+++ src/store/user/types.ts
@@ -1,11 +1,18 @@
 export type User = {
-  token: string | null;
+  token: string;
+  id: number | null;
   firstName: string | null;
   lastName: string | null;
   email: string | null;
 };
 
-export type Action = {
-  type: string;
-  payload: any;
-};
+export type Action =
+  | {
+      type: "LOGIN_SUCCESS";
+      payload: User;
+    }
+  | {
+      type: "TOKEN_STILL_VALID";
+      payload: any;
+    }
+  | { type: "LOG_OUT" };
