c6b5492ffa38d33cfba12216f295eb1e5769eb9d
ariqyandri <ariq140199@gmail.com>
2020-11-26 11:02:35 +0100

    Add temporary logout button on home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectDummy } from "../../store/dummy/selectors";
+import { logOut } from "../../store/user/action";
 
 export default function Home() {
   const dummy = useSelector(selectDummy);
-  return <div>{dummy.attr1}</div>;
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logOut());
+  };
+
+  return (
+    <div>
+      {dummy.attr1}
+      <div>
+        <button onClick={handleLogout}>log out</button>
+      </div>
+    </div>
+  );
 }
