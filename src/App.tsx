f64e577dbe0908fe95b5a649adca02d8f005049c
ariqyandri <ariq140199@gmail.com>
2020-11-26 12:21:07 +0100

    Uninstall bootstrap and react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Home from "./pages/Home/index";
 import SignUp from "./pages/SignUp/index";
 import Login from "./pages/Login/index";
 import Writings from "./pages/Writings/index";
+import Navbar from "./components/Navbar/index";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/writings" component={Writings} />
