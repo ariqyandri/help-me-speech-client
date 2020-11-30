2cacc4fab9ef3e56167cdc6b451bb9df31be0317
ariqyandri <ariq140199@gmail.com>
2020-11-30 18:53:24 +0100

    Update Navbar component

---
+++ src/components/Navbar/LoggedIn.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Button from "react-bootstrap/Button";
+import { selectUser } from "../../store/user/selectors";
+import Nav from "react-bootstrap/Nav";
+import { useHistory } from "react-router-dom";
+import { logOut } from "../../store/user/action";
+import NavbarItem from "./NavbarItem";
+
+export default function LoggedIn() {
+  const history = useHistory();
+
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  const handleLogOut = (e: any) => {
+    e.preventDefault();
+    dispatch(logOut());
+    return history.push("/");
+  };
+
+  return (
+    <>
+      <NavbarItem path="/mywritings" linkText="My Writings" />
+      <NavbarItem path="/writing/create" linkText="Create Writing" />
+      <Nav.Item style={{ padding: ".5rem 1rem" }}>{user.email}</Nav.Item>
+      <Button onClick={handleLogOut}>Logout</Button>
+    </>
+  );
+}
--- src/components/Navbar/LoggedOut.ts
+++
@@ -1,9 +0,0 @@
-import { NavLink } from "./type";
-
-const navLinks: NavLink = [
-    { id: 1, title: "Home", path: "/" },
-    { id: 2, title: "Login", path: "/login" },
-    { id: 3, title: "Sign Up", path: "/signup" },
-  ];
-
-export const LoggedOut = navLinks;
