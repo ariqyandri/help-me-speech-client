7cd83cf96e0aecb3af9a3ab219d5c583d80e8bfc
ariqyandri <ariq140199@gmail.com>
2020-12-01 15:48:41 +0100

    Make signup form a component

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,19 +1,10 @@
-import React, { useState, useEffect } from "react";
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
+import React, { useEffect } from "react";
 import { selectToken } from "../../store/user/selectors";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory, Link } from "react-router-dom";
-import { Col } from "react-bootstrap";
-import { signUp } from "../../store/user/action";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import SignupForm from "../../components/SignupForm/index";
 
-export default function SignUp() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const dispatch = useDispatch();
+export default function Login() {
   const token = useSelector(selectToken);
   const history = useHistory();
 
@@ -23,68 +14,9 @@ export default function SignUp() {
     }
   }, [token, history]);
 
-  function submitForm(event: any) {
-    event.preventDefault();
-
-    dispatch(signUp(firstName, lastName, email, password));
-
-    setFirstName("");
-    setLastName("");
-    setEmail("");
-    setPassword("");
-  }
-
   return (
-    <Container>
-      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
-        <h1 className="mt-5 mb-5">Signup</h1>
-        <Form.Group controlId="formBasicName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control
-            value={firstName}
-            onChange={(event) => setFirstName(event.target.value)}
-            type="text"
-            placeholder="Enter first name"
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            value={lastName}
-            onChange={(event) => setLastName(event.target.value)}
-            type="text"
-            placeholder="Enter last name"
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            type="email"
-            placeholder="Enter email"
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            type="password"
-            placeholder="Password"
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mt-5">
-          <Button variant="primary" type="submit" onClick={submitForm}>
-            Sign up
-          </Button>
-        </Form.Group>
-        <Link to="/login">Click here to log in</Link>
-      </Form>
-    </Container>
+    <div>
+      <SignupForm />
+    </div>
   );
 }
