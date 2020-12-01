1ef9f1c6f726d4deb337e2ecbefb333232a876b4
ariqyandri <ariq140199@gmail.com>
2020-12-01 15:03:34 +0100

    Update home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,14 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectDummy } from "../../store/dummy/selectors";
-import { logOut } from "../../store/user/action";
 
 export default function Home() {
   const dummy = useSelector(selectDummy);
-  const dispatch = useDispatch();
-
-  const handleLogout = () => {
-    dispatch(logOut());
-  };
 
   return (
     <div>
       {dummy.attr1}
-      <div>
-        <button onClick={handleLogout}>log out</button>
-      </div>
+      Home
     </div>
   );
 }
