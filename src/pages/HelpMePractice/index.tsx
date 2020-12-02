dec99ded4b312d5b648ba71008a7d82f13be26db
ariqyandri <ariq140199@gmail.com>
2020-12-02 19:25:39 +0100

    Install ts-stopwatch and debug help me page

--- src/pages/HelpMePractice/index.tsx
+++ src/pages/HelpMePractice/index.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import DisplayMyWritingPractice from "../../components/DisplayWritingPractice/index";
 import Loading from "../../components/Loading";
+import { selectToken } from "../../store/user/selectors";
 import { fetchWriting } from "../../store/writing/action";
 import { selectWriting } from "../../store/writing/selector";
 import { Params } from "./types";
 
 export default function HelpMePractice() {
   const dispatch = useDispatch();
+  const token = useSelector(selectToken);
+  const history = useHistory();
+  if (!token) {
+    history.push("/");
+  }
   const params: Params = useParams();
   const id: number = parseInt(params.id);
   const writing = useSelector(selectWriting);
