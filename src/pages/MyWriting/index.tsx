f105b7542c8a501f6900736d31d93e0c56dbb8b1
ariqyandri <ariq140199@gmail.com>
2020-12-02 05:29:53 +0100

    Fetch writing on my writing page

--- src/pages/MyWriting/index.tsx
+++ src/pages/MyWriting/index.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchWriting } from "../../store/writing/action";
+import { Params } from "./types";
 
 export default function MyWriting() {
+  const dispatch = useDispatch();
+  const params: Params = useParams();
+  const id: number = parseInt(params.id);
+  useEffect(() => {
+    dispatch(fetchWriting(id));
+  }, [dispatch]);
   return <div>My Writing</div>;
 }
