0845209e4af534a99e91abe957c57624988bef14
ariqyandri <ariq140199@gmail.com>
2020-12-02 08:23:46 +0100

    Debug edit writing page

--- src/pages/EditWriting/index.tsx
+++ src/pages/EditWriting/index.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import EditWritingForm from "../../components/EditWritingForm/index";
 import Loading from "../../components/Loading";
 import { selectToken } from "../../store/user/selectors";
+import { fetchWriting } from "../../store/writing/action";
 import { selectWriting } from "../../store/writing/selector";
 import { Writing, Params } from "./types";
 
 export default function EditWriting() {
+  const dispatch = useDispatch();
   const token = useSelector(selectToken);
   const history = useHistory();
   if (!token) {
@@ -15,6 +17,9 @@ export default function EditWriting() {
   }
   const params: Params = useParams();
   const id: number = parseInt(params.id);
+  useEffect(() => {
+    dispatch(fetchWriting(id));
+  }, [dispatch, id]);
   const writing = useSelector(selectWriting);
   if (!writing) {
     return <Loading />;
