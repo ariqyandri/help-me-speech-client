bcf21a02f666123a26d4d3a4b7326c1e91d27d96
ariqyandri <ariq140199@gmail.com>
2020-12-03 21:05:51 +0100

    Debug and display image preview

--- src/components/ImagePreview/index.tsx
+++ src/components/ImagePreview/index.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectImages } from "../../store/images/selector";
+import { ImageType } from "./types";
 
 export default function ImagePreview() {
   const images = useSelector(selectImages);
-  return <div></div>;
+  console.log(images);
+  if (images.find((image: ImageType) => image.id === 0)) {
+    return <div>{null}</div>;
+  }
+  return (
+    <div>
+      {images.map((image: ImageType) => {
+        return <img src={image.url} alt={image.name} />;
+      })}
+    </div>
+  );
 }
