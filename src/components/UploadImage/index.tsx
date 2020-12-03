3b04c9d5eb89217d97f1ae9786601d1878992e1a
ariqyandri <ariq140199@gmail.com>
2020-12-03 11:52:25 +0100

    Add cloudinary upload widget on component

---
+++ src/components/UploadImage/index.tsx
@@ -0,0 +1,25 @@
+//@ts-nocheck
+import React from "react";
+import { Button } from "react-bootstrap";
+
+export default function UploadImage() {
+  const handleOpen = async () => {
+    const widget = cloudinary.createUploadWidget(
+      {
+        cloudName: "doai9yryh",
+        uploadPreset: "dgfhe4a6",
+      },
+      (error: any, result: any) => {
+        if (result.event === "success") {
+          console.log(`success`, result.info.url);
+        }
+      }
+    );
+    widget.open();
+  };
+  return (
+    <div>
+      <Button onClick={handleOpen}></Button>
+    </div>
+  );
+}
