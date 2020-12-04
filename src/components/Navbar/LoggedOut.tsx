607c5841b61927ad19634ce85014aa3140d30454
ariqyandri <ariq140199@gmail.com>
2020-12-04 05:53:52 +0100

    Debug warning

--- src/components/Navbar/LoggedOut.tsx
+++ src/components/Navbar/LoggedOut.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import NavbarItem from "./NavbarItem";
 
 export default function LoggedOut() {
   const history = useHistory();
-  const dispatch = useDispatch();
   const handleLogIn = (e: any) => {
     e.preventDefault();
     history.push("/login");
