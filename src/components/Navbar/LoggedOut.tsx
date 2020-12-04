f293dba4417f24f824a5e04f38f87a6bd8314b7c
ariqyandri <ariq140199@gmail.com>
2020-12-04 05:50:45 +0100

    Update navbar

--- src/components/Navbar/LoggedOut.tsx
+++ src/components/Navbar/LoggedOut.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import NavbarItem from "./NavbarItem";
 
 export default function LoggedOut() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const handleLogIn = (e: any) => {
+    e.preventDefault();
+    history.push("/login");
+  };
   return (
     <>
-      <NavbarItem path="/login" linkText="Login" />
+      <NavbarItem path="/login" linkText="Create Writing" />
+      <Button onClick={handleLogIn}>Login</Button>
     </>
   );
 }
