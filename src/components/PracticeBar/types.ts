7bf70ae75e13ad98f555573b4e43e3e1215a6cd6
ariqyandri <ariq140199@gmail.com>
2020-12-17 15:10:02 +0100

    Update new

---
+++ src/components/PracticeBar/types.ts
@@ -0,0 +1,18 @@
+export type Writing = {
+  id: number;
+  title: string;
+  content: string;
+  isPrivate: boolean;
+  imageUrl: string | null;
+  videoUrl: string | null;
+  userId: number;
+  categoryId: number;
+  category: Category;
+  description: string | null;
+  createdAt: any;
+  updatedAt: any;
+};
+export type Category = {
+  name: string;
+};
+export type Props = { myWriting: Writing };
