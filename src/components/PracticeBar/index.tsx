9067155157a88b5ea71c194a8c9b58e99597886f
ariqyandri <ariq140199@gmail.com>
2020-12-17 15:15:13 +0100

    Debug

--- src/components/PracticeBar/index.tsx
+++ src/components/PracticeBar/index.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { Badge, Card } from "react-bootstrap";
-import { useSelector } from "react-redux";
-import { selectAppLoading } from "../../store/appState/selectors";
-import Loading from "../Loading";
 import "./DisplayMyWritingPractice.css";
-import { Link } from "react-router-dom";
 
 export default function DisplayMyWritingPractice() {
   return <div></div>;
