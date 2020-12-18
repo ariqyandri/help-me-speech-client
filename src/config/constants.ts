b1e72db75cd8b1636e2df3d542fb37efb4c7ac95
ariqyandri <ariq140199@gmail.com>
2020-12-18 12:03:45 +0100

    Update api url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,5 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl =
+  process.env.API_URL || "https://help-me-speech.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const displayTime = (s: number) => {
   const sec = Math.floor(s % 60);
